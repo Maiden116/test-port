ac9f2868be2a335e9a848f076916fab45cb4866a
Emmanuel Leiva <emma_lthe@hotmail.com>
2020-10-01 23:04:55 -0300

    functions push add empty completed

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var path = require('path');
-
 var app = express();
+const bodyParser = require('body-parser')
 
 app.use(express.static(path.join(__dirname, 'view')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.listen(8000, '0.0.0.0', function() {});
+
+app.listen(8000, '0.0.0.0', function() {
+    console.log("escuchando puerto 8000");
+});
+
+
+
 
 module.exports = app;
--- view/js/script.js
+++ view/js/script.js
@@ -1,1 +1,34 @@
 // Add your javascript here
+var buttons = $(":button")
+
+buttons.on("click", function(e){    
+    actionHandler(e.target.value, $("#inputElem").val())
+    
+  });
+
+function actionHandler(action, value) {
+    var stackBoardItem = $(".stack-item");
+    var stackBoard = $(".stack-board");
+    if(value == undefined || value == null || value == ""){
+        return alert("You must type a value on the input")
+    }
+     var maxIndex = 0 
+     stackBoardItem.each(function(index,element){
+        maxIndex = Math.max(maxIndex,element.id)
+     })
+    switch (action) {
+        case "Push" : {
+            stackBoard.prepend('<div class="stack-item" id="'+(maxIndex+1)+'">'+value+'</div>');
+            break;
+        }
+        case "Pop":{
+            stackBoard.children("#"+maxIndex).remove();
+            break;
+        }
+        case "Empty": {
+            stackBoard.children(".stack-board").remove();
+        }
+
+    }
+}
+
